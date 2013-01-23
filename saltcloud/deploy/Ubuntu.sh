5452facdd7e9bbebddd45d61395be6654880707c
pcrews <gleebix@gmail.com>
2013-01-23 12:30:37 -0800

    Further modifications of the Ubuntu.sh script

--- saltcloud/deploy/Ubuntu.sh
+++ saltcloud/deploy/Ubuntu.sh
@@ -5,9 +5,11 @@ echo '{{ vm['priv_key'] }}' > /etc/salt/pki/minion.pem
 echo '{{ vm['pub_key'] }}' > /etc/salt/pki/minion.pub
 echo "{{ minion }}" > /etc/salt/minion
 
-echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | tee /etc/apt/sources.list.d/saltstack.list
-wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | apt-key add -
 apt-get update
+apt-get install -y python-software-properties
+echo | add-apt-repository  ppa:saltstack/salt
+apt-get update
+apt-get  -o Dpkg::Options::="--force-confdef" -o DPkg::Options::="--force-confold" -y install salt-minion
 
-apt-get install -y -o Dpkg::Options::="--force-confdef" -o DPkg::Options::=--force-confold salt-minion
-# minion will be started automatically by install
+service salt-minion start
+
